c2bbc020e70a1e223f5bd6911e7a76d1615a1376
Moira Renata <moirarenata.s@gmail.com>
2024-12-02 21:26:21 -0800

    update constrasts

--- rf_analysis.R
+++ rf_analysis.R
@@ -5,7 +5,7 @@ data$max_depth[is.na(data$max_depth)] <- 0
 
 
 data$Block <- factor(data$Block, levels = c("Split 1 (70/30)", "Split 2 (80/20)", "Split 3 (90/10)"))
-data$n_estimators <- factor(data$n_estimators, levels = c("5", "100", "500"))
+data$n_estimators <- factor(data$n_estimators, levels = c("1", "100", "500"))
 data$max_depth <- factor(data$max_depth, levels = c("0", "1", "10"))
 
 data_anova <- aov(Accuracy ~ Block + n_estimators + max_depth + 
@@ -15,22 +15,24 @@ print(summary(data_anova))
 
 ## box-cox transformation 
 library(MASS)
+par(mar = c(5, 4, 4, 2) + 0.1)
 boxcox(Accuracy ~ Block + n_estimators + max_depth, data = data, 
        lambda = seq(-1, 2, len = 27), ylab = "Log Likelihood")
 
 ## contrasts
 LowVersusHighEstimators <- c(-1, 0, 1)
-NoDepthVersusHighDepth <- c(-1, 0, 1)
-NoDepthVersusMidDepth <- c(-1, 1, 0)
-MidDepthVersusHighDepth <- c(0, -1, 1)
+MedVersusHighEstimators <- c(0, -1, 1)
+InfDepthVersusHighDepth <- c(-1, 0, 1)
+InfDepthVersusMidDepth <- c(-1, 1, 0)
 
-contrasts(data$n_estimators) <- cbind(LowVersusHighEstimators)
-contrasts(data$max_depth) <- cbind(NoDepthVersusHighDepth, NoDepthVersusMidDepth)
+contrasts(data$n_estimators) <- cbind(LowVersusHighEstimators, MedVersusHighEstimators)
+contrasts(data$max_depth) <- cbind(InfDepthVersusHighDepth, InfDepthVersusMidDepth)
 
 data_contrasts_anova <- aov(Accuracy ~ Block + n_estimators + max_depth + 
                               n_estimators:max_depth, data = data)
 
 summary(data_contrasts_anova, 
-        split = list(n_estimators = list(LowVersusHighEstimators = 1),
-                     max_depth = list(NoDepthVersusHighDepth = 1, NoDepthVersusMidDepth = 2)))
+        split = list(n_estimators = list(LowVersusHighEstimators = 1, MedVersusHighEstimators = 2),
+                     max_depth = list(InfDepthVersusHighDepth = 1, InfDepthVersusMidDepth = 2)))
 
+coef(summary.lm(data_contrasts_anova))
